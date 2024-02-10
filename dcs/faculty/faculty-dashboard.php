0abf3ee4431530ce23d61143a5c5eae09c0546d4
keanuribs <keanuribs04@gmail.com>
2024-02-10 10:41:29 +0800

    -facultyandstudwithinclude

--- dcs/faculty/faculty-dashboard.php
+++ dcs/faculty/faculty-dashboard.php
@@ -15,13 +15,13 @@
 <body className='snippet-body'>
 
     <body id="body-pd">
-        <?php require '../include/sidebar.php';?>
+        <?php require 'include/sidebar.php';?>
         <!--Container Main start-->
         <div class="height-100 bg-light">
             <h4>Main Components</h4>
         </div>
         <!--Container Main end-->
-        <?php require '../include/footer.php';?>
+        <?php require 'include/footer.php';?>
     </body>
 
 </html>
